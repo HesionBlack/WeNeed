59b5657d5897e0349a14cac8ab853721cad91493
HesionBlack <1026320617@qq.com>
2020-02-06 16:30:35 +0800

    修改flyway的建表语句

--- src/main/resources/db/migration/V17__Create_v_orderExample_view.sql
+++ src/main/resources/db/migration/V17__Create_v_orderExample_view.sql
@@ -1,3 +1,3 @@
 create view  v_orderexample
 as
-select `order`.`id` AS `id`,`order`.`buyerId` AS `buyerId`,`order`.`providerId` AS `providerId`,`order`.`questionId` AS `questionId`,`order`.`description` AS `description`,`order`.`number` AS `number`,`order`.`createTime` AS `createTime`,`order`.`del_flag` AS `del_flag`,`user`.`name` AS `pName` from (`order` left join `user` on((`user`.`id` = `order`.`buyerId`)))
+select `order`.`id` AS `id`,`order`.`buyerId` AS `buyerId`,`order`.`providerId` AS `providerId`,`order`.`questionId` AS `questionId`,`order`.`description` AS `description`,`order`.`number` AS `number`,`order`.`createTime` AS `createTime`,`order`.`del_flag` AS `del_flag`,`user`.`name` AS `pName` from (`order` left join `user` on((`user`.`id` = `order`.`providerId`)))
