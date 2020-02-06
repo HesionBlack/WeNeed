2132e060e17bc3e4d9bbba4a827c21510c8baccd
HesionBlack <1026320617@qq.com>
2020-02-06 15:57:08 +0800

    修改了flyway建表语句

--- src/main/resources/db/migration/V16__Create_order_table.sql
+++ src/main/resources/db/migration/V16__Create_order_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `order` (
-  `id` varchar(100) NOT NULL,
-  `buyerId` varchar(36) DEFAULT NULL,
-  `providerId` varchar(36) DEFAULT NULL,
-  `questionId` bigint DEFAULT NULL,
-  `description` message_text DEFAULT NULL,
-  `number` int DEFAULT NULL,
-  `createTime` datetime DEFAULT NULL,
-  `del_flag` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) 
+create table `order`
+(
+    id varchar(36)  primary key not null,
+    buyerId varchar(36),
+    providerId varchar(36),
+    questionId bigint,
+    description LONGTEXT,
+    number int,
+    createTime date ,
+    del_flag varchar(10)
+);
